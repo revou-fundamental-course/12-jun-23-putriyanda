fe44a5c75fc2d30b52478e106b7e91ead86d3958
Yanchannn <136346781+putriyanda@users.noreply.github.com>
2023-06-22 14:58:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("convert").onclick = Convert;
+document.getElementById("clear").onclick = clear;
+
+function Convert() {
+  var fahrenheit = document.getElementById("fahrenheit").value;
+  var celsius = document.getElementById("celsius").value;
+
+  if (fahrenheit != "") {
+    celsius = (parseFloat(fahrenheit) - 32) / 1.8;
+  } else {
+    fahrenheit = parseFloat(celsius) * 1.8 + 32;
+  }
+
+  document.getElementById("fahrenheit").value =
+    parseFloat(fahrenheit).toFixed(1);
+  document.getElementById("celsius").value = parseFloat(celsius).toFixed(1);
+}
+
+function clear() {
+  document.getElementById("fahrenheit").value = "";
+  document.getElementById("celsius").value = "";
+}
